287cfdd77a1edf3c583991c7ddb10bc9ff8eb3f0
danijourdain <danijourdain@icloud.com>
2022-05-10 12:15:49 -0600

    Create May10.cpp

---
+++ Tutorials/May10.cpp
@@ -0,0 +1,18 @@
+/* ASSIGNMENTS
+ * written parts: clear and concise
+ * MUST RUN ON U OF C LINUX COMPUTERS
+ * provide comments and documentation on where you get code from
+ *  - if from Dr. Federl, its fine, just cite him
+ *  - comment code on what you are doing
+ *  - if the question says you cannot use external resources, DON'T and don't cite them
+ * start assignments immediately once its out
+ *  - first one will be easier, but remaining ones will not be
+ * 
+ * PLAGARISM
+ * all assignments are individual work
+ * don't look for code online & copy paste
+ *  - code will be run against a plagarism checker with previous year's assignments
+ * don't discuss implementation, only high level ideas
+ * do not record or write anything when discussing, and wait 20 minutes after discussion to start typing code
+ */
+
