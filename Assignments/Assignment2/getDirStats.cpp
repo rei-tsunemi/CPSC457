a30017ef0a68f2fb8160c4419ff9a7c83be58fc7
danijourdain <danijourdain@icloud.com>
2022-05-25 17:34:10 -0600

    pls for the love of god work

--- Assignments/Assignment2/getDirStats.cpp
+++ Assignments/Assignment2/getDirStats.cpp
@@ -14,8 +14,8 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <dirent.h>
+#include <vector>
 #include <iostream>
-#include <cstring>
 
 #define TRIVIAL_DIR(buf) (((buf)[0] == '.' && (buf)[1] == '\0') || ((buf)[0] == '.' && (buf)[1] == '.' && (buf)[2] == '\0'))
 
@@ -53,7 +53,53 @@ Results getDirStats(const std::string & dir_name, int n)
   res.largest_file_size = 0;
   //also initilalize the values for other Results values
 
-  /* code for opening and reading through a directory adapted from May 17 tutorial code */
+  std::vector<std::string> d_names;
+  d_names.insert(0, dir_name);
+
+  while(!d_names.empty()) {
+    std::string name = d_names.pop_back();
+    std::cout<<name<<std::endl;
+    DIR* dir;
+    dir = opendir(name.c_str());
+    //open the current directory
+
+    struct dirent* dirContent;
+    bool isDir;
+
+    while((dirContent = readdir(dir)) != NULL) {
+      isDir = dirContent->d_type == DT_DIR;
+
+      if(isDir) {
+        if(TRIVIAL_DIR(dirContent->d_name)) {
+        continue;
+        }
+
+        res.n_dirs++;
+        std::string sub_name = dirContent->d_name;
+        d_names.insert(0, sub_name);
+      }
+      else {
+        res.n_files++;
+        //char pathname[4096];
+        //getcwd(pathname, 4096);
+        std::cout<<"file: "<<dirContent->d_name<<std::endl;
+      }
+    }
+  }
+
+  return res;
+
+  /*
+  res.n_dirs = 0;
+  res.n_files = 0;
+  //set the number of files and directories to 0 to start
+
+  res.largest_file_path = "";
+  res.all_files_size = 0;
+  res.largest_file_size = 0;
+  //also initilalize the values for other Results values
+
+  /* code for opening and reading through a directory adapted from May 17 tutorial code *
   DIR* dir;
   dir = opendir(dir_name.c_str());
   //open the current directory
@@ -97,98 +143,8 @@ Results getDirStats(const std::string & dir_name, int n)
   }
 
   closedir(dir);
-
-  /*
-  res.n_dirs = 0;
-  res.n_files = 0;
-  //since the top directory is not included in the number of directories,
-  //set both the number of files and number of directories to 0 to begin
-
-  res.largest_file_size = 0;
-  res.all_files_size = 0;
-  //also initialize the largest and total file size to 0
-
-  res.largest_file_path = "";
-
-  DIR* directory;
-  directory = opendir(".");
-  struct dirent* contents;
-  contents = readdir(directory);
-
-  std::cout << "Point 1" << std::endl;
-
-  while(contents != NULL) {
-    char* pathname;
-//    pathname = getcwd(pathname, 0);
-    bool isDir = is_dir(pathname);
-    //check if the current directory entry is another directory
-    std::cout << "Point 2" << std::endl;
-
-    if(isDir) {
-      std::string name = contents->d_name;
-      bool isTrivial = (name.compare(".") == 0) || ( name.compare("..") == 0);
-      //if the current directory entry is another directory, first check if it is trivial or not
-
-      if(isTrivial) {
-        continue;
-        //if it is a trivial directory, skip this iteration of the loop
-      }
-      
-      res.n_dirs++;
-      //since a non trivial directory was found, increment the number of directories
-
-      Results dirRes = getDirStats(name, n);
-      //recursively get the directory stats for this sub-directory
-
-      if(dirRes.largest_file_size > res.largest_file_size) {
-        res.largest_file_size = dirRes.largest_file_size;
-        //if the sub-directory held a larger file than the previous largest file,
-        //change the largest_file_size value
-
-        std::string longestPath = dirRes.largest_file_path;
-        longestPath.insert(0, name);
-        //insert the current directory name to the largest file path
-      }
-      
-      res.n_dirs += dirRes.n_dirs;
-      res.n_files += dirRes.n_files;
-      //add the number of files and directories the directory has
-      //to the current values
-    }
-    else {
-      std::cout << "Point 3" << std::endl;
-      res.n_files++;
-      
-      struct stat* buf;
-      int read = stat(pathname, buf);
-      //attemt to read the file data using stat
-      if(read != 0) {
-        std::cout << "Error reading file" << std::endl;
-        exit(1);
-        //if the read fails, terminate the program
-      }
-
-      std::cout << "Point 4" << std::endl;
-
-
-      if(buf->st_size > res.largest_file_size) {
-        //if the current file is largest than the previous largest file,
-        //replace the largest file data
-        res.largest_file_size = buf->st_size;
-        res.largest_file_path = contents->d_name;
-      }      
-
-      res.all_files_size += buf->st_size;
-      //add the current file size to the total file size
-
-      
-    }
-
-    contents = readdir(directory);
-  }
-
-  closedir(directory);
   */
+
   /*
   if (! is_dir(dir_name)) return res;
 
@@ -220,5 +176,4 @@ Results getDirStats(const std::string & dir_name, int n)
   res.duplicate_files.push_back(group2);
   */
 
-  return res;
 }
