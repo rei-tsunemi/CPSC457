4b2e82ca2a1598a02f5afbae2df3cbf19cb2f003
danijourdain <danijourdain@icloud.com>
2022-05-25 20:22:33 -0600

    iterative version??

--- Assignments/Assignment2/getDirStats.cpp
+++ Assignments/Assignment2/getDirStats.cpp
@@ -39,9 +39,6 @@ static bool is_dir(const std::string & path)
 //
 Results getDirStats(const std::string & dir_name, int n)
 {
-  // The results below are all hard-coded, to show you all the fields
-  // you need to calculate. You should delete all code below and
-  // replace it with your own code.
   Results res;
 
   res.n_dirs = 0;
@@ -53,88 +50,79 @@ Results getDirStats(const std::string & dir_name, int n)
   res.largest_file_size = 0;
   //also initilalize the values for other Results values
 
-  /* code for opening and reading through a directory adapted from May 17 tutorial code */
-  DIR* dir;
-  dir = opendir(dir_name.c_str());
-  //open the current directory
-  if(!dir) {
-    std::cout<<"Unable to open directory"<<std::endl;
-    exit(1);
-  }
+  std::vector<std::string> directories;
+  directories.push_back(dir_name);
+  //create a stack for traversing through the directories and push the starting dir_name to it
 
-  struct dirent* dirContent;
-  bool isDir;
+  while (!directories.empty()) {
+    std::string d_name = directories.back();
+    std::cout<<d_name<<std::endl;
+    directories.pop_back();
+    //take the top entry out of the stack
 
-  while((dirContent = readdir(dir)) != NULL) {
-    isDir = (dirContent->d_type == DT_DIR);
-    //check if the current content is a directory
-
-    if(isDir) {
-      if(TRIVIAL_DIR(dirContent->d_name)) {
-        continue;
-        //if the current item is a trivial directory, skip this iteration
-      }
-
-      res.n_dirs++;
-      //increment the number of other directories
-
-      chdir(dir_name.c_str());
-      //change the working directory to the new folder
-
-      Results dirRes = getDirStats(dirContent->d_name, n);
-      //recursively get the directory stats for any sub-directories
-
-      if(dirRes.largest_file_size > res.largest_file_size) {
-        //if the sub-directory found a larger file, update the results
-        res.largest_file_size = dirRes.largest_file_size;
-        std::string longestPath = dirRes.largest_file_path;
-        longestPath.insert(0, "/");
-        longestPath.insert(0, dir_name);
-        res.largest_file_path = longestPath;
-      }
-
-      res.n_dirs += dirRes.n_dirs;
-      res.n_files += dirRes.n_files;
-      res.all_files_size += dirRes.all_files_size;
-      //add the stats found in the sub-directory
+    /* code for opening and reading through a directory adapted from May 17 tutorial code */
+    DIR* dir;
+    dir = opendir(dir_name.c_str());
+    //open the current directory
+    if(!dir) {
+      std::cout<<"Unable to open directory"<<std::endl;
+      exit(1);
     }
-    else {
-      chdir(dir_name.c_str());
-      res.n_files++;
-      //change to the current directory and increment the number of files
 
-      char pathname[4096];
-      getcwd(pathname, 4096);
-      //get the pathname to the current spot
+    struct dirent* dirContent;
+    bool isDir;
 
-      std::string full_name = pathname;
-      full_name.append("/");
-      full_name.append(dirContent->d_name);
-      //add the file name to the end of the path
+    while((dirContent = readdir(dir)) != NULL) {
+      isDir = dirContent->d_type == DT_DIR;
+      //check if the current content is a directory
 
-      struct stat stats;
-      int result = stat(full_name.c_str(), &stats);
-      if(result != 0) {
-        exit(1);
+      if(isDir) {
+        if(TRIVIAL_DIR(dirContent->d_name)) {
+          continue;
+          //if the current item is a trivial directory, skip this iteration
+        }
+
+        res.n_dirs++;
+        std::string full_name = d_name;
+        full_name.append("/");
+        full_name.append(dirContent->d_name);
+        directories.push_back(full_name);
+        //increment the number of directories found and add the pathname starting at dir_name to the directories stack
       }
-      //use stat to get data about the file
+      else {
+        res.n_files++;
 
-      chdir("..");
-      res.all_files_size += stats.st_size;
-      //return to the previous directory and add to the total file size
+        char pathname[4096];
+        getcwd(pathname, 4096);
+        //get the pathname to the current spot
 
-      if(stats.st_size > res.largest_file_size) {
-        res.largest_file_size = stats.st_size;
-        std::string long_path = dir_name;
-        long_path.append("/");
-        long_path.append(dirContent->d_name);
-        res.largest_file_path = long_path;
-        //if the current file is the largest, adjust accordingly
+        std::string full_name = pathname;
+        full_name.append("/");
+        full_name.append(dirContent->d_name);
+        //add the file name to the end of the path
+
+        struct stat stats;
+        int result = stat(full_name.c_str(), &stats);
+        if(result != 0) {
+          exit(1);
+        }
+        //use stat to get data about the file
+
+        res.all_files_size += stat.st_size;
+
+        if(stats.st_size > res.largest_file_size) {
+          res.largest_file_size = stats.st_size;
+          std::string long_path = d_name;
+          long_path.append("/");
+          long_path.append(dirContent->d_name);
+          res.largest_file_path = long_path;
+          //if the current file is the largest, adjust accordingly
+        }
       }
     }
+    
+    closedir(dir);
   }
 
-  closedir(dir);
-  chdir("..");
   return res;
 }
