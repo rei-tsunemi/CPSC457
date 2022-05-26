d9aa6c4e9ee5314cd8a651cce4f59feb5ec79391
danijourdain <danijourdain@icloud.com>
2022-05-25 19:47:32 -0600

    'easy' part donegit add Assignments/Assignment2/getDirStats.cpp !

--- Assignments/Assignment2/getDirStats.cpp
+++ Assignments/Assignment2/getDirStats.cpp
@@ -53,72 +53,34 @@ Results getDirStats(const std::string & dir_name, int n)
   res.largest_file_size = 0;
   //also initilalize the values for other Results values
 
-  std::vector<std::string> d_names;
-  d_names.insert(0, dir_name);
-
-  while(!d_names.empty()) {
-    std::string name = d_names.pop_back();
-    std::cout<<name<<std::endl;
-    DIR* dir;
-    dir = opendir(name.c_str());
-    //open the current directory
-
-    struct dirent* dirContent;
-    bool isDir;
-
-    while((dirContent = readdir(dir)) != NULL) {
-      isDir = dirContent->d_type == DT_DIR;
-
-      if(isDir) {
-        if(TRIVIAL_DIR(dirContent->d_name)) {
-        continue;
-        }
-
-        res.n_dirs++;
-        std::string sub_name = dirContent->d_name;
-        d_names.insert(0, sub_name);
-      }
-      else {
-        res.n_files++;
-        //char pathname[4096];
-        //getcwd(pathname, 4096);
-        std::cout<<"file: "<<dirContent->d_name<<std::endl;
-      }
-    }
-  }
-
-  return res;
-
-  /*
-  res.n_dirs = 0;
-  res.n_files = 0;
-  //set the number of files and directories to 0 to start
-
-  res.largest_file_path = "";
-  res.all_files_size = 0;
-  res.largest_file_size = 0;
-  //also initilalize the values for other Results values
-
-  /* code for opening and reading through a directory adapted from May 17 tutorial code *
+  /* code for opening and reading through a directory adapted from May 17 tutorial code */
   DIR* dir;
   dir = opendir(dir_name.c_str());
   //open the current directory
+  if(!dir) {
+    std::cout<<"Unable to open directory"<<std::endl;
+    exit(1);
+  }
 
   struct dirent* dirContent;
   bool isDir;
 
   while((dirContent = readdir(dir)) != NULL) {
     isDir = (dirContent->d_type == DT_DIR);
+    //check if the current content is a directory
 
     if(isDir) {
       if(TRIVIAL_DIR(dirContent->d_name)) {
         continue;
+        //if the current item is a trivial directory, skip this iteration
       }
-      std::cout<<dirContent->d_name<<std::endl;
 
       res.n_dirs++;
-      char* name = dirContent->d_name;
-      closedir(dir);
+      //increment the number of other directories
+
+      chdir(dir_name.c_str());
+      //change the working directory to the new folder
+
       Results dirRes = getDirStats(dirContent->d_name, n);
       //recursively get the directory stats for any sub-directories
 
@@ -126,54 +88,47 @@ Results getDirStats(const std::string & dir_name, int n)
         //if the sub-directory found a larger file, update the results
         res.largest_file_size = dirRes.largest_file_size;
         std::string longestPath = dirRes.largest_file_path;
-        longestPath.insert(0, dirContent->d_name);
+        longestPath.insert(0, "/");
+        longestPath.insert(0, dir_name);
         res.largest_file_path = longestPath;
       }
 
       res.n_dirs += dirRes.n_dirs;
       res.n_files += dirRes.n_files;
+      res.all_files_size += dirRes.all_files_size;
       //add the stats found in the sub-directory
     }
     else {
+      chdir(dir_name.c_str());
       res.n_files++;
-      //char pathname[4096];
-      //getcwd(pathname, 4096);
-      std::cout<<"file: "<<dirContent->d_name<<std::endl;
+
+      char pathname[4096];
+      getcwd(pathname, 4096);
+
+      std::string full_name = pathname;
+      full_name.append("/");
+      full_name.append(dirContent->d_name);
+
+      struct stat stats;
+      int result = stat(full_name.c_str(), &stats);
+      if(result != 0) {
+        exit(1);
+      }
+
+      chdir("..");
+      res.all_files_size += stats.st_size;
+
+      if(stats.st_size > res.largest_file_size) {
+        res.largest_file_size = stats.st_size;
+        std::string long_path = dir_name;
+        long_path.append("/");
+	long_path.append(dirContent->d_name);
+        res.largest_file_path = long_path;
+      }
     }
   }
 
   closedir(dir);
-  */
-
-  /*
-  if (! is_dir(dir_name)) return res;
-
-  // prepare a fake results
-  res.largest_file_path = dir_name + "/some_dir/some_file.txt";
-  res.largest_file_size = 123;
-  res.n_files = 321;
-  res.n_dirs = 333;
-  res.all_files_size = 1000000;
-
-  std::string type1 = "C source";
-  int count1 = 5;
-  res.most_common_types.push_back({ type1, count1 });
-  res.most_common_types.push_back({ "makefile script", 4 });
-  res.most_common_types.push_back({ "C++ source", 2 });
-  res.most_common_types.push_back({ "PNG image", 1 });
-
-  res.most_common_words.push_back({ "hello", 3 });
-  res.most_common_words.push_back({ "world", 1 });
-
-  std::vector<std::string> group1;
-  group1.push_back(dir_name + "/file1.cpp");
-  group1.push_back(dir_name + "/lib/sub/other.c");
-  res.duplicate_files.push_back(group1);
-  std::vector<std::string> group2;
-  group2.push_back(dir_name + "/readme.md");
-  group2.push_back(dir_name + "/docs/readme.txt");
-  group2.push_back(dir_name + "/x.y");
-  res.duplicate_files.push_back(group2);
-  */
-
+  chdir("..");
+  return res;
 }
