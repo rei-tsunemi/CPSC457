b778d352509f5814d073468613da587c057b5458
danijourdain <danijourdain@icloud.com>
2022-06-03 14:59:30 -0600

    completed hint 3

--- Assignments/Assignment3/detect-primes/detectPrimes.cpp
+++ Assignments/Assignment3/detect-primes/detectPrimes.cpp
@@ -21,41 +21,6 @@
 
 #define MAX_THREADS 256
 
-// C++ barrier class (from lecture notes).
-// -----------------------------------------------------------------------------
-// You do not have to use it in your implementation. If you don't use it, you
-// may delete it.
-class simple_barrier {
-	std::mutex m_;
-	std::condition_variable cv_;
-	int n_remaining_, count_;
-	bool coin_;
-
-	public:
-	simple_barrier(int count = 1) { init(count); }
-	void init(int count)
-	{
-		count_ = count;
-		n_remaining_ = count_;
-   	coin_ = false;
-	}
-	bool wait()
-	{
-		if (count_ == 1) return true;
-		std::unique_lock<std::mutex> lk(m_);
-		if (n_remaining_ == 1) {
-		coin_ = ! coin_;
-		n_remaining_ = count_;
-		cv_.notify_all();
-		return true;
-		}
-		auto old_coin = coin_;
-		n_remaining_--;
-		cv_.wait(lk, [&]() { return old_coin != coin_; });
-		return false;
-	}
-};
-
 struct ThreadData {
 	int start, n_threads, thread_num;
 	std::vector<int64_t>* nums;
@@ -74,31 +39,9 @@ std::vector<int64_t> primes;
 int64_t n;
 bool results[MAX_THREADS];
 
-/*
-// returns true if n is prime, otherwise returns false
-// -----------------------------------------------------------------------------
-// to get full credit for this assignment, you will need to adjust or even
-// re-write the code in this function to make it multithreaded.
-static bool is_prime(int64_t n)
-{
-	// handle trivial cases
-	if (n < 2) return false;
-	if (n <= 3) return true; // 2 and 3 are primes
-	if (n % 2 == 0) return false; // handle multiples of 2
-	if (n % 3 == 0) return false; // handle multiples of 3
-	// try to divide n by every number 5 .. sqrt(n)
-	int64_t i = 5;
-	int64_t max = sqrt(n);
-	while (i <= max) {
-   	if (n % i == 0) return false;
-   	if (n % (i + 2) == 0) return false;
-   	i += 6;
-	}
-	// didn't find any divisors, so it must be a prime
-	return true;
-}
-*/
 
+// returns true if n is prime, otherwise returns false
+// ----------------------------------------------------------------------------
 static bool is_prime(int64_t n, struct ThreadData d) {
 	if(n < 2) return false;
 	if (n <= 3) return true;
