64af6a6cc24d9e3677663d3c968c7fa686caefdd
danijourdain <danijourdain@icloud.com>
2022-06-08 16:38:01 -0600

    actually used the helper functions....

--- Assignments/Assignment4/deadlock-detect/deadlock_detector.cpp
+++ Assignments/Assignment4/deadlock-detect/deadlock_detector.cpp
@@ -5,51 +5,6 @@
 #include <unordered_map>
 #include <iostream>
 
-std::string my_trim(std::string s) {
-	std::string whitespace = " \n\r\t\f\v";
-	size_t start, end, range;
-	start = s.find_first_not_of(whitespace);
-	end = s.find_last_not_of(whitespace);
-	//find the start and end position of the actual word, ignoring whitespace
-
-	range = end - start + 1;
-	return s.substr(start, range);
-}
-
-std::vector<std::string> my_split(std::string s) {
-	std::string delim1 = "->";
-	std::string delim2 = "<-";
-	std::vector<std::string> result;
-	std::string str;
-
-	size_t pos = 0;
-	if((pos = s.find(delim1)) != std::string::npos) {
-		//if the arrow is pointing right, trim the string to get the two words and push them both to the front of the vector
-		str = s.substr(0, pos);
-		str.push_back('p');	//add a p to the end of the string to indicate that it is a process
-		result.push_back(str);
-
-		s.erase(0, pos + 2);
-		//s = my_trim(s);
-		s.push_back('r');	//add an r to the end of the string to indicate that it is a resource
-		result.push_back(s);
-	}
-	else if((pos = s.find(delim2)) != std::string::npos) {
-		//if the arrow points left, trim the string but put both parts to the back of the vector
-		str = s.substr(0, pos);
-		str.push_back('p');	//add a p to the end of the string to indicate that it is a process
-		//result.push_back(str);
-
-		s.erase(0, pos + 2);
-		//s = my_trim(s);
-		s.push_back('r');	//add an r to the end of the string to indicate that it is a process
-		result.push_back(s);
-		result.push_back(str);
-	}
-
-	return result;
-}
-
 class Graph {
 private:
 	std::unordered_map<std::string, std::vector<std::string>> adj_list;
@@ -57,13 +12,23 @@ private:
 
 public:
 	void add(std::string e) {
-		std::vector<std::string> r = my_split(e);
+		std::vector<std::string> r = split(e);
 		//split the string into the starting and end point of the edge. Ex. "2 <- a" would be split into "a" and "2" with "a" being the first element
 
 		std::string start, end;
-		start = r.at(0);
-		end = r.at(1);
-		//get the starting and end points of the edge
+		if(r.at(1).compare("->") == 0) {
+			start = r.at(0);
+			start.push_back('p');
+			end = r.at(2);
+			end.push_back('r');
+			//add a p and r to the appropriate strings to indicate a process and resource respectively
+		}
+		else {
+			start = r.at(2);
+			start.push_back('r');
+			end = r.at(0);
+			end.push_back('p');
+		}
 
 		out_counts[start]++;
 		//increment the number of edges coming from the start point
@@ -73,11 +38,12 @@ public:
 			std::vector<std::string> temp;
 			temp.emplace_back(start);
 			adj_list.insert({end, temp});
-			out_counts[end];
+			out_counts[end] = 0;
 			//if end is not already in the map, create a vector of strings and insert it into the map
+			//also initialize out_counts to 0
 		}
 		else {
-			(found->second).emplace_back(start);
+			(found->second).push_back(start);
 			//otherwise, just add the new element
 		}
 
@@ -121,7 +87,6 @@ public:
 			std::string s = i.first;
 			if(s.back() == 'p') {
 				s.pop_back();
-				s = my_trim(s);
 				result.push_back(s);
 			}
 		}
