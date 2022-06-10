677aa4a5c63b115600b520a81d34371661a999d2
danijourdain <danijourdain@icloud.com>
2022-06-10 10:52:08 -0600

    tweaked a few things

--- Assignments/Assignment4/scheduler/scheduler.cpp
+++ Assignments/Assignment4/scheduler/scheduler.cpp
@@ -28,39 +28,32 @@ void simulate_rr(
 ) {
 	seq.clear();
 	std::vector<int> rq, jq;
-	int64_t cur_time = 0;
+	int64_t cur_time = 0, remaining_slice = quantum;
 	int cpu = -1;
 	std::vector<int64_t> remaining_bursts;
-	int64_t remaining_slice = quantum - 1;
 
 	for(long unsigned int i = 0; i < processes.size(); i++) {
 		jq.push_back(i);
 		remaining_bursts.push_back(processes.at(i).burst);
 	}
 
-	bool context_switch = false;
 	while(1) {
 		//std::cout<<"TIME = "<<cur_time<<std::endl;
 		if(rq.empty() && jq.empty() && cpu == -1) {
-			if(seq.size() > max_seq_len) {
-				seq.resize(max_seq_len);
-			}
 			break;
 			//if there are no processes in either the job queue or ready queue, and the cpu is idle, the simulation is done, so break
 		}
 
-		if(seq.empty() || seq.back() != cpu) {
+		if((seq.empty() || seq.back() != cpu) && seq.size() < max_seq_len) {
 			seq.push_back(cpu);
+			//if a different process is running than what was running previously, add it to the execution sequence
 		}
-
-		remaining_slice = quantum - 1;
-		//reset the quantum
 
 		//add any process that came before the current time
 		while(!jq.empty()) {
 			int i = jq.front();
 			if(processes.at(i).arrival_time < cur_time) {
-			//	std::cout<<i<<"th process added to rq"<<std::endl;
+				//std::cout<<i<<"th process added to rq"<<std::endl;
 				rq.push_back(i);
 				jq.erase(jq.begin());
 				//if the process's start time has come, add it to the ready queue and remove it from the job queue
@@ -88,7 +81,7 @@ void simulate_rr(
 		while(!jq.empty()) {
 			int i = jq.front();
 			if(processes.at(i).arrival_time == cur_time) {
-			//	std::cout<<i<<"th process added to rq"<<std::endl;
+				//std::cout<<i<<"th process added to rq"<<std::endl;
 				rq.push_back(i);
 				jq.erase(jq.begin());
 				//if the process's start time has come, add it to the ready queue and remove it from the job queue
@@ -103,8 +96,7 @@ void simulate_rr(
 			//if the CPU is idle, and there are processes ready, take one from the ready queue
 			cpu = rq.at(0);
 			rq.erase(rq.begin());
-			context_switch = false;
-		//	std::cout<<"switched to process # "<<cpu<<std::endl;
+			//std::cout<<"switched to process # "<<cpu<<std::endl;
 
 			if(processes.at(cpu).start_time == -1) {
 				processes.at(cpu).start_time = cur_time;
@@ -112,31 +104,7 @@ void simulate_rr(
 			}
 		}
 
-	//	std::cout<<"RUNNING PROCESS # "<<cpu<<std::endl;
-
-	/*	if(cpu != -1) {
-			//if the cpu is not idle, adjust the remaining burst time for the running process
-			//remaining_bursts.at(cpu)--;
-
-			if(remaining_bursts.at(cpu) == 0) {
-				//std::cout<<"finished process"<<std::endl;
-				processes.at(cpu).finish_time = cur_time;
-				context_switch = true;
-				//if the process is finished, set the end time and set the cpu to idle
-			}
-
-			//std::cout<<remaining_slice<<" time left until switch"<<std::endl;
-			if(remaining_slice == 0 && (rq.empty() || rq.at(0) != cpu)) {
-				context_switch = true;
-				//if the quantum expires, signal to the simulation that a context switch is needed
-			}
-
-			remaining_slice--;
-		}
-		else {
-			context_switch = true;
-			//if the cpu is idle, attempt to perform a context switch
-		} */
+		//std::cout<<"RUNNING PROCESS # "<<cpu<<std::endl;
 
 		if(cpu == -1 && !jq.empty()) {
 			cur_time = processes.at(jq.at(0)).arrival_time;
@@ -145,17 +113,17 @@ void simulate_rr(
 			cur_time++;
 		}
 		else if(remaining_bursts.at(cpu) <= quantum) {
-		//	std::cout<<"time left for process: "<<remaining_bursts.at(cpu)<<std::endl;
+			//std::cout<<"time left for process: "<<remaining_bursts.at(cpu)<<std::endl;
 			cur_time += remaining_bursts.at(cpu);
 			remaining_bursts.at(cpu) = 0;
 
 		}
 		else {
-		//	std::cout<<"time left for process: "<<remaining_bursts.at(cpu)<<std::endl;
+			//std::cout<<"time left for process: "<<remaining_bursts.at(cpu)<<std::endl;
 			cur_time += quantum;
 			remaining_bursts.at(cpu) -= quantum;
 		}
-		//cur_time++;
-//		std::cout<<std::endl;
+
+		//std::cout<<std::endl;
 	}
 }
