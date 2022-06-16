6427b3b91d64db20d95caa0aa9ad492f7be10157
danijourdain <danijourdain@icloud.com>
2022-06-15 20:08:01 -0600

    correct output for everything except test6 which doesnt run lmao

--- Assignments/Assignment5/memsim/memsim.cpp
+++ Assignments/Assignment5/memsim/memsim.cpp
@@ -10,9 +10,11 @@
 #include <iostream>
 #include <list>
 #include <algorithm>
+#include <cmath>
 
 struct Partition {
-	int64_t address, size, tag;
+	int64_t address, size;
+	int tag;
 
 	Partition() {
 		size = 0;
@@ -26,45 +28,89 @@ struct Partition {
 		tag = -1;
 	}
 
-	Partition(int64_t a, int64_t s, int64_t t) {
+	Partition(int64_t a, int64_t s, int t) {
 		address = a;
 		size = s;
 		tag = t;
 	}
+
+	void print() {
+		std::cout<<"Printing partition with tag "<<tag<<":\n";
+		std::cout<<"address = "<<address<<std::endl;
+		std::cout<<"size = "<<size<<"\n";
+	}
 };
+
+typedef std::list<Partition>::iterator ParIt;
 
 // I recommend you implement the simulator as a class. This is only a suggestion.
 // If you decide not to use this class, please remove it before submitting it.
-class Simulator {
-	private:
+struct Simulator {
 	std::list<Partition> partitions;
 	int64_t page_size;
 	int64_t pages_requested;
 
-	public:
 	Simulator(int64_t page_size) {
 		this->page_size = page_size;
 		pages_requested = 0;
 	}
 
 	void allocate(int tag, int size) {
-		std::list<Partition>::iterator it;
-		int64_t max_size = 0, max_address;
+		ParIt it, max_it;
+		int64_t max_size = 0, max_address = 0;
 
 		for(it = partitions.begin(); it != partitions.end(); ++it) {
 			if(it->tag == -1 && it->size > max_size) {
 				max_size = it->size;
 				max_address = it->address;
+				max_it = it;
 			}
 		}
 
-		Partition temp(max_address, max_size);
-		it = std::find(partitions.begin(), partitions.end(), temp);
-		temp.size -= size;
-		temp.address += size;
-		partitions.insert(std::next(it), temp);
-		it->size = size;
-		it->tag = tag;
+		if(max_size < size) {
+			int64_t addr;
+
+			if(!partitions.empty()) {
+				addr = partitions.back().address + partitions.back().size;
+			}
+			else {
+				addr = 0;
+			}
+
+			int n_pages = 0;
+			if(partitions.back().tag == -1) {
+				n_pages = ceil((size - partitions.back().size) / (double)page_size);
+				partitions.back().size += n_pages * page_size;
+				//if the last partition is empty, just increase its size
+			}
+			else {
+				n_pages = ceil(size / (double)page_size);
+				Partition newPar(addr, (n_pages * page_size));
+				partitions.emplace_back(newPar);
+				//otherwise create a new partition of n_pages pages and add it to the end of the list
+			}
+			pages_requested += n_pages;
+			//update the number of requested pages
+
+			max_size = partitions.back().size;
+			max_address = partitions.back().address;
+			max_it = std::prev(partitions.end());
+			//update the values for the largest partition
+		}
+
+		if(max_size > size) {
+			Partition full(max_address, size, tag);
+			partitions.insert(max_it, full);
+			max_it->address += size;
+			max_it->size -= size;
+			//create a new partition to hold the requested memory and insert it in the left side of the remaining memory
+			//also update the remaining memory in the free partition
+		}
+		else {
+			max_it->tag = tag;
+			//if the chosen partition is the exact size requested, just change the tag of the partition
+
+		}
 
     // Pseudocode for allocation request:
     // - search through the list of partitions from start to end, and
@@ -81,6 +127,31 @@ class Simulator {
 	}
 
 	void deallocate(int tag) {
+		ParIt it = partitions.begin();
+		while(1) {
+			if(it == partitions.end()) break;
+			if(it == partitions.end()) return;
+
+			if(it->tag == tag) {
+				it->tag = -1;
+
+				ParIt temp = std::next(it);
+				if(temp->tag == -1) {
+					it->size += temp->size;
+					partitions.erase(temp);
+				}
+
+				temp = std::prev(it);
+				if(temp->tag == -1) {
+					temp->size += it->size;
+					partitions.erase(it);
+					it = temp;
+				}
+			}
+
+			++it;
+		}
+
     // Pseudocode for deallocation request:
     // - for every partition
     //     - if partition is occupied and has a matching tag:
@@ -108,6 +179,12 @@ class Simulator {
  	}
 
   	void check_consistency() {
+		ParIt it;
+		for(it = partitions.begin(); it != partitions.end(); ++it) {
+			it->print();
+		}
+		std::cout<<std::endl;
+
     // you do not need to implement this method at all - this is just my suggestion
     // to help you with debugging
 
@@ -155,7 +232,7 @@ MemSimResult mem_sim(int64_t page_size, const std::vector<Request> & requests)
 		else {
 			sim.allocate(req.tag, req.size);
 		}
-		sim.check_consistency();
+		//sim.check_consistency();
 	}
 
 	return sim.getStats();
